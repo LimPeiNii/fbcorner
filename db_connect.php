3af0bdeb99cd6d604995544242f426faf0352b91
LimPeiNii <73421685+LimPeiNii@users.noreply.github.com>
2022-06-22 15:23:57 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,10 @@
 <?php
 
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
+    $server = 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
+    $username = 'vpa9ltfk8f2klijc';
+    $password = 'i5szwdaz2f1wps43';
 
-    $db = 'fbcorner';
+    $db = 'idhuce2uhpwhzlsk';
 
     // Create connection
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
